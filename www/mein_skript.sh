46a29bd772701681a15c0603731ac068088f7f25
Andre Heller <andreheller@me.com>
2016-03-10 10:42:01 +0100

    Revert "Neue Daten"
    This reverts commit c460b63573cb49e71e2eb31e587d313759681941.

--- www/mein_skript.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Test"
-raspistill -t 1000 -o /var/www/html/share/www/img/shellBild.jpg
--- www/shoot.php
+++
@@ -1,3 +0,0 @@
-<?php
-shell_exec("raspistill -t 1000 -o /var/www/img/meinBild.jpg");
-php?>
